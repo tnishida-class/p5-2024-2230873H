ab4889667410f8a80cc12802e0d74e27fc435857
2230873H <2230873h@stu.kobe-u.ac.jp>
2024-11-21 01:13:49 +0900

    section4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -7,12 +7,32 @@ function setup(){
   // 配列をランダムに初期化する
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = random(70, 100); // 60以上100未満のランダムな数を代入
   }
 
   // 円グラフを描くには割合が必要なので合計を計算しておく
   let total = 0;
   for(let i = 0; i < scores.length; i++){ total += scores[i]; }
-
+  console.log(total);
   // BLANK[1]
-}
+
+  fill(179,18,98);
+
+  arc(200, 200, 150, 150, 0, TWO_PI*scores[1]/total,PIE);
+  arc(200, 200, 150, 150, TWO_PI*scores[1]/total, TWO_PI*scores[1]/total+TWO_PI*scores[2]/total,PIE);
+  arc(200, 200, 150, 150,  TWO_PI*scores[1]/total+TWO_PI*scores[2]/total, TWO_PI*scores[1]/total+TWO_PI*scores[2]/total+TWO_PI*scores[3]/total,PIE);
+  arc(200, 200, 150, 150,  TWO_PI*scores[1]/total+TWO_PI*scores[2]/total+TWO_PI*scores[3]/total, TWO_PI*scores[1]/total+TWO_PI*scores[2]/total+TWO_PI*scores[3]/total+TWO_PI*scores[4]/total,PIE);
+  arc(200, 200, 150, 150,  TWO_PI*scores[1]/total+TWO_PI*scores[2]/total+TWO_PI*scores[3]/total+TWO_PI*scores[4]/total, TWO_PI*scores[1]/total+TWO_PI*scores[2]/total+TWO_PI*scores[3]/total+TWO_PI*scores[4]/total+TWO_PI*scores[5]/total,PIE);
+  arc(200, 200, 150, 150,  TWO_PI*scores[1]/total+TWO_PI*scores[2]/total+TWO_PI*scores[3]/total+TWO_PI*scores[4]/total+TWO_PI*scores[5]/total, TWO_PI*scores[1]/total+TWO_PI*scores[2]/total+TWO_PI*scores[3]/total+TWO_PI*scores[4]/total+TWO_PI*scores[5]/total+TWO_PI*scores[6]/total,PIE);
+  arc(200, 200, 150, 150,  TWO_PI*scores[1]/total+TWO_PI*scores[2]/total+TWO_PI*scores[3]/total+TWO_PI*scores[4]/total+TWO_PI*scores[5]/total+TWO_PI*scores[6]/total, TWO_PI*scores[1]/total+TWO_PI*scores[2]/total+TWO_PI*scores[3]/total+TWO_PI*scores[4]/total+TWO_PI*scores[5]/total+TWO_PI*scores[6]/total+TWO_PI*scores[7]/total,PIE);
+  arc(200, 200, 150, 150,  TWO_PI*scores[1]/total+TWO_PI*scores[2]/total+TWO_PI*scores[3]/total+TWO_PI*scores[4]/total+TWO_PI*scores[5]/total+TWO_PI*scores[6]/total+TWO_PI*scores[7]/total, TWO_PI*scores[1]/total+TWO_PI*scores[2]/total+TWO_PI*scores[3]/total+TWO_PI*scores[4]/total+TWO_PI*scores[5]/total+TWO_PI*scores[6]/total+TWO_PI*scores[7]/total+TWO_PI*scores[8]/total,PIE);
+  arc(200, 200, 150, 150,  TWO_PI*scores[1]/total+TWO_PI*scores[2]/total+TWO_PI*scores[3]/total+TWO_PI*scores[4]/total+TWO_PI*scores[5]/total+TWO_PI*scores[6]/total+TWO_PI*scores[7]/total+TWO_PI*scores[8]/total, TWO_PI*scores[1]/total+TWO_PI*scores[2]/total+TWO_PI*scores[3]/total+TWO_PI*scores[4]/total+TWO_PI*scores[5]/total+TWO_PI*scores[6]/total+TWO_PI*scores[7]/total+TWO_PI*scores[8]/total+TWO_PI*scores[9]/total,PIE);
+  arc(200, 200, 150, 150,  TWO_PI*scores[1]/total+TWO_PI*scores[2]/total+TWO_PI*scores[3]/total+TWO_PI*scores[4]/total+TWO_PI*scores[5]/total+TWO_PI*scores[6]/total+TWO_PI*scores[7]/total+TWO_PI*scores[8]/total+TWO_PI*scores[9]/total,0,PIE);
+
+
+
+
+
+  }
+
+
